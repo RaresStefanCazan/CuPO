bef54db600aed01bd9f4b2daccc806d509cc5ceb
RaresStefanCazan <stefanrarescazan@gmail.com>
2024-05-25 18:51:04 +0300

    PHP

--- TEST/controllers/pages-controller.php
+++ TEST/controllers/pages-controller.php
@@ -1,19 +1,32 @@
 <?php
-class HomeController {
-    public function index() {
-        require_once '../views/Html/HomePage.html';
+    // Controller pentru pagina "Recipes"
+    class PagesController {
+        public function recipes() {
+            require_once('/xampp/htdocs/CuPO/WEB/TEST/views/Html/Recipes.html');
+        }
+        public function shoppingList() {
+            require_once('/xampp/htdocs/CuPO/WEB/TEST/views/Html/ShoppingList.html');
+        }
     }
-}
 
-class RecipesController {
-    public function index() {
-        require_once '../views/Html/Recipes.html';
-    }
-}
+// Creăm o instanță a controllerului
+$controller = new PagesController();
 
-class CulinaryPreferencesController {
-    public function index() {
-        require_once '../views/Html/CulinaryPreferences.html';
+// Verificăm dacă 'page' este setat în URL
+if (isset($_GET['page'])) {
+    // Obținem pagina din URL
+    $page = $_GET['page'];
+
+    // Verificăm dacă metoda există în controller
+    if (method_exists($controller, $page)) {
+        // Apelăm metoda
+        $controller->$page();
+    } else {
+        // Dacă metoda nu există, afișăm o eroare sau redirecționăm către o pagină de eroare
+        echo "Pagina nu a fost găsită";
     }
+} else {
+    // Dacă 'page' nu este setat, putem redirecționa către o pagină implicită
+    $controller->recipes();
 }
 ?>
