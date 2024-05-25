22858bbaac0f027db24b4a215c0738fba6940c78
RaresStefanCazan <stefanrarescazan@gmail.com>
2024-05-26 00:45:56 +0300

    abj
    Am adaugat index.php
    la poze in html-uri mai trebuie schimbat path-ul, n am mai schimbat la toate
    si la fel la unele pagini s ar putea sa nu mearga cateva butoane ex home ca la fel trebuie schimbat path-ul ca daca te uiti in pages controller.php tot path-ul de la home e doar home acum si peste tot unde ai buton de home trebuie pus /home/home

--- TEST/controllers/pages-controller.php
+++ TEST/controllers/pages-controller.php
@@ -1,41 +1,45 @@
 <?php
-// Controller pentru pagina "Recipes"
+
 class PagesController {
-    public function recipes() {
-        echo "Loading Recipes Page";
-        // Path to the controller itself, might need to include specific recipe logic file if exists
-        require_once __DIR__ . '/../views/Html/Recipes.html';
-    }
-
-    public function shoppingList() {
-        echo "Loading Shopping List Page";
-        require_once __DIR__ . '/../views/Html/ShoppingList.html';
-    }
-
-    
+    private $pages = [
+        'home' => '/../views/Html/HomePage.html',
+        'recipes' => '/../views/Html/Recipes.html',
+        'shoppingList' => '/../views/Html/ShoppingList.html',
+        'statistics' => '/../views/Html/Statistics.html',
+        'metadataManagement' => '/../views/Html/MetadataManagement.html',
+        'admin' => '/../views/Html/Admin.html',
+        'login' => '/../views/Html/Login.html',
+        'register' => '/../views/Html/Register.html',
+        'forgot' => '/../views/Html/ForgotPassword.html',
+        'userInfo' => '/../views/Html/UserInfo.html',
+        'info' => '/../views/Html/Info.html',
+    ];
 
     public function handleRequest() {
-        // Verificăm dacă 'page' este setat în URL
-        if (isset($_GET['page'])) {
-            // Obținem pagina din URL
-            $page = $_GET['page'];
+        $page = $_GET['page'] ?? 'recipes';
 
-            // Verificăm dacă metoda există în controller
-            if (method_exists($this, $page)) {
-                // Apelăm metoda
-                $this->$page();
-            } else {
-                // Dacă metoda nu există, afișăm o eroare sau redirecționăm către o pagină de eroare
-                echo "Pagina nu a fost găsită";
-            }
+        if (array_key_exists($page, $this->pages)) {
+            return $this->pages[$page];
         } else {
-            // Dacă 'page' nu este setat, putem redirecționa către o pagină implicită
-            $this->recipes();
+            throw new Exception("Pagina nu a fost găsită");
         }
     }
 }
 
-// Creăm o instanță a controllerului și gestionăm cererea
-$controller = new PagesController();
-$controller->handleRequest();
+class View {
+    public function render($page) {
+        require_once __DIR__ . $page;
+    }
+}
+
+try {
+    $controller = new PagesController();
+    $view = new View();
+
+    $page = $controller->handleRequest();
+    $view->render($page);
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
+
 ?>
---
+++ TEST/index.php
@@ -0,0 +1,14 @@
+<?php
+require_once 'controllers/pages-controller.php';
+
+// Creăm o instanță a controllerului și gestionăm cererea
+$controller = new PagesController();
+$view = new View();
+
+try {
+    $page = $controller->handleRequest();
+    $view->render($page);
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
+?>
